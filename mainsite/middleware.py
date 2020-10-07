758356405b19a0b72a2e8a939a4fa0961f8f20f1
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2020-10-07 09:46:40 -0700

    Remove slash

--- mainsite/middleware.py
+++ mainsite/middleware.py
@@ -4,5 +4,5 @@ class CorsMiddleware(object):
 
     def __call__(self, request):
         response = self.get_response(request)
-        response["Access-Control-Allow-Origin"] = "lcog-hr-frontend.s3-website-us-west-2.amazonaws.com/"
+        response["Access-Control-Allow-Origin"] = "lcog-hr-frontend.s3-website-us-west-2.amazonaws.com"
         return response

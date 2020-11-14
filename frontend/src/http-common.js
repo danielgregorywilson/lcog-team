d07e61d4461d93d2034e514dea8110df0df3153b
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2020-11-13 16:38:39 -0800

    Remove default manager token from all API requests

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: process.env.API_URL,
   headers: {
-    'Content-type': 'application/json',
-    'Authorization': 'Token 05c5ab7d90b00e4278ec37ffb8394953e4a8c97e'
+    'Content-type': 'application/json'
   }
 });

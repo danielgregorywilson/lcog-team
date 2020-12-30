f3c64c9e0ff92cbe37345f2b07527cf72d1b918b
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2020-12-29 17:31:43 -0800

    Login test

---
+++ frontend/cypress/integration/examples/login.spec.js
@@ -0,0 +1,17 @@
+describe('Login Page', () => {
+  it('successfully loads', () => {
+    cy.visit('/')
+  })
+  it('accepts credentials', () => {
+    cy.get('#username')
+      .type('dwilson')
+      .should('have.value', 'dwilson')
+    cy.get('#password')
+      .type('fank9crax.SEAP0scuh')
+      .should('have.value', 'fank9crax.SEAP0scuh')
+  })
+  it('logs in successfully', () => {
+    cy.contains('Login').click()
+    cy.url().should('include', '/dashboard')
+  })
+})

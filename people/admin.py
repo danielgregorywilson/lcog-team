8b437a86e901f164e582392144f4aef7ec35384a
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2021-08-20 10:53:12 -0700

    Admin with privacy group

--- people/admin.py
+++ people/admin.py
@@ -47,8 +47,35 @@ class PerformanceReviewAdmin(admin.ModelAdmin):
     list_display = ("username", "status", "effective_date")
     inlines = (SignatureInline, SignatureReminderInline)
 
+    def get_form(self, request, obj=None, **kwargs):
+        if hasattr(obj, 'pk') and not request.user.is_superuser and obj.pk not in request.user.employee.prs_can_view():
+            self.exclude = (
+                'step_increase', 'top_step_bonus', 'action_other',
+                'factor_job_knowledge', 'factor_work_quality',
+                'factor_work_quantity', 'factor_work_habits',
+                'factor_analysis', 'factor_initiative', 'factor_interpersonal',
+                'factor_communication', 'factor_dependability',
+                'factor_professionalism', 'factor_management',
+                'factor_supervision', 'evaluation_successes',
+                'evaluation_opportunities', 'evaluation_goals_manager',
+                'evaluation_goals_employee', 'evaluation_comments_employee',
+                'description_reviewed_employee', 'signed_position_description'
+            )
+        else:
+            self.exclude = ()
+        form = super(PerformanceReviewAdmin, self).get_form(request, obj, **kwargs)
+        return form
+
 
 @admin.register(ReviewNote)
 class ReviewNoteAdmin(admin.ModelAdmin):
     list_display = ("manager", "employee", "date")
     readonly_fields = ("date",)
+
+    def get_form(self, request, obj=None, **kwargs):
+        if hasattr(obj, 'pk') and not request.user.is_superuser and obj.pk not in request.user.employee.notes_can_view():
+            self.exclude = ('note',)
+        else:
+            self.exclude = ()
+        form = super(ReviewNoteAdmin, self).get_form(request, obj, **kwargs)
+        return form

03091cfaa4ee72035bc5404ad089c005b9274d78
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2021-10-04 17:53:47 -0700

    More permissions updates

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -253,7 +253,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny'
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,

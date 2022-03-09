69cc0ed88e313c2f74eba99942a0946dc4316241
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2022-03-09 12:29:17 -0800

    Show all tags for the responsibilities of a given tag in tag detail pages

--- responsibilities/serializers.py
+++ responsibilities/serializers.py
@@ -4,51 +4,6 @@ from rest_framework.response import Response
 from .models import Responsibility, Tag
 
 
-class SimpleResponsibilitySerializer(serializers.HyperlinkedModelSerializer):
-    # For use by TagSerializer - does not have url or tags
-
-    primary_employee_pk = serializers.SerializerMethodField()
-    primary_employee_name = serializers.SerializerMethodField()
-    secondary_employee_pk = serializers.SerializerMethodField()
-    secondary_employee_name = serializers.SerializerMethodField()
-    
-    class Meta:
-        model = Responsibility
-        fields = [
-            'pk', 'name', 'description', 'link', 'primary_employee_pk',
-            'primary_employee_name', 'secondary_employee_pk',
-            'secondary_employee_name'
-        ]
-
-    @staticmethod
-    def get_primary_employee_pk(responsibility):
-        if responsibility.primary_employee:
-            return responsibility.primary_employee.pk
-        else:
-            return ''
-
-    @staticmethod
-    def get_secondary_employee_pk(responsibility):
-        if responsibility.secondary_employee:
-            return responsibility.secondary_employee.pk
-        else:
-            return ''
-
-    @staticmethod
-    def get_primary_employee_name(responsibility):
-        if responsibility.primary_employee:
-            return responsibility.primary_employee.user.get_full_name()
-        else:
-            return ''
-    
-    @staticmethod
-    def get_secondary_employee_name(responsibility):
-        if responsibility.secondary_employee:
-            return responsibility.secondary_employee.user.get_full_name()
-        else:
-            return ''
-
-
 class SimpleTagSerializer(serializers.HyperlinkedModelSerializer):
     # For use by ResponsibilitySerializer - does not have url or responsibilities
     
@@ -110,7 +65,7 @@ class TagSerializer(serializers.HyperlinkedModelSerializer):
 
     def get_responsibilities(self, tag):
         responsibilities = tag.responsibility_set.all()
-        serializer = SimpleResponsibilitySerializer(
+        serializer = ResponsibilitySerializer(
             responsibilities, many=True, context=self.context
         )
         return serializer.data

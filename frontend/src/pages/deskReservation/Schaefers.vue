2ce733e635478206f8a4d6ee6929e803bdffb83d
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2022-05-13 17:32:35 -0700

    Remove desk reservation page refresh timer because the kiosks Tony set up refresh themselves every 5 or so minutes

--- frontend/src/pages/deskReservation/Schaefers.vue
+++ frontend/src/pages/deskReservation/Schaefers.vue
@@ -4,7 +4,6 @@
       <div>
         <router-view></router-view>
       </div>
-      <div class="row justify-center" id="refresh-timer">Time to refresh: {{ count }}</div>
     </div>
   </q-page>
 </template>
@@ -26,20 +25,5 @@ import { Component, Vue } from 'vue-property-decorator'
 
 @Component
 export default class Schaefers extends Vue{
-  // TODO: Remove. This is a temporary timer to refresh the page every 10 minutes so we get updates from other screens.
-  private count: number = 60 * 5
-
-  mounted() {
-    // TODO: Remove timer.
-    // var el = document.getElementById('time-to-update');
-    setInterval(() => {
-        this.count -= 1;
-        // el.innerHTML = count;
-        if (this.count <= 0) {
-            location.reload();
-        }
-    }, 1000);
-  }
-
 }
 </script>

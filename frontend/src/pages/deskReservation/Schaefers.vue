219a248571f78faffdb08e3d7ee714a33c270013
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2022-05-13 17:52:22 -0700

    Show a screensaver on desk reservation page before 6AM and after 6PM

--- frontend/src/pages/deskReservation/Schaefers.vue
+++ frontend/src/pages/deskReservation/Schaefers.vue
@@ -5,6 +5,21 @@
         <router-view></router-view>
       </div>
     </div>
+
+    <q-dialog
+      v-model="showScreensaver"
+      :maximized="true"
+      transition-show="slide-up"
+      transition-hide="slide-down"
+      @mousemove="mouseMoveOverScreensaver"
+    >
+      <q-card class="row items-center justify-center">
+        <q-card-section>
+          <div class="text-h6 row justify-center">Oh, I'm sorry. We weren't expecting you.</div>
+          <div class="text-h6 row justify-center">Please, move the mouse to wake.</div>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -21,5 +36,17 @@ import { Component, Vue } from 'vue-property-decorator'
 
 @Component
 export default class Schaefers extends Vue{
+  private showScreensaver = false
+
+  mouseMoveOverScreensaver() {
+    this.showScreensaver = false
+  }
+
+  mounted() {
+    const hourOfDay = new Date().getHours()
+    if (hourOfDay < 6 || hourOfDay >= 18) {
+      this.showScreensaver = true
+    }
+  }
 }
 </script>

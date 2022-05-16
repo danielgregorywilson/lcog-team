c26cc9a912a786b579f310aa3b2f64f69115348f
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2022-05-16 06:06:12 -0700

    Add active field to Employee

--- people/models.py
+++ people/models.py
@@ -69,6 +69,7 @@ class Employee(models.Model):
     def username(self):
         return self.user.username
 
+    active = models.BooleanField(default=True)
     user = models.OneToOneField("auth.User", verbose_name=_("user"), on_delete=models.CASCADE)
     manager = models.ForeignKey("self", related_name="direct_reports", blank=True, null=True, verbose_name=_("manager"), on_delete=models.SET_NULL)
     unit_or_program = models.ForeignKey("people.UnitOrProgram", verbose_name=_("unit/program"), on_delete=models.SET_NULL, blank=True, null=True)

92853e373a4311d2357b67ce58bbb8a0d8d8b716
Dan Wilson <daniel.gregory.wilson@gmail.com>
2022-11-04 16:53:03 -0700

    Add timestamps to logging statements

--- timeoff/management/commands/send_test_timeoff_next_week_report.py
+++ timeoff/management/commands/send_test_timeoff_next_week_report.py
@@ -1,14 +1,18 @@
+from datetime import datetime
+
 from django.core.management.base import BaseCommand
 
 from timeoff.helpers import send_test_timeoff_next_week_report
+
 
 class Command(BaseCommand):
     help = 'Sends next week time off email to members of the IS team'
 
     def handle(self, *args, **options):
         num_tor_days, num_employees = send_test_timeoff_next_week_report()
+        dt = datetime.now()
         if num_tor_days == 0:
-            message = f'No time off notifications sent to the test unit.'
+            message = f'{dt} - No time off notifications sent to the test unit.'
         else:
-            message = f'Sent notification of {num_tor_days} time off requests to {num_employees} test employees.'
+            message = f'{dt} - Sent notification of {num_tor_days} time off requests to {num_employees} test employees.'
         self.stdout.write(self.style.SUCCESS(message))

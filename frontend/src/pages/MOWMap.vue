dd4ebe6df059f96c429c5bd8c86169deccd661c2
Dan Wilson <daniel.gregory.wilson@gmail.com>
2023-02-22 16:51:39 -0800

    Set up MapBox: add points to map with colors; hide/show with checkboxes

--- frontend/src/pages/MOWMap.vue
+++ frontend/src/pages/MOWMap.vue
@@ -11,12 +11,13 @@
                 :options="routeOptions"
                 type="checkbox"
                 v-model="selectedRoutes"
+                @input="updateMapVisibility"
               />
             </div>
             <q-checkbox v-model="showWaitlisted" label="Show Waitlisted" class="row" />
             <q-btn color="primary" label="Print selected" class="row q-mt-md" />
           </div>
-          <iframe id="map" class="q-mt-md" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-123.01851868629457%2C44.046311231687326%2C-123.01467776298524%2C44.05306625383836&amp;layer=mapnik&amp;marker=44.04968883906406%2C-123.01659822463989" style="border: 1px solid black"></iframe>
+          <div id="map"></div>
         </div>
         <q-input v-model="newAddress" label="Add a Delivery Address">
           <template v-slot:prepend>
@@ -45,21 +46,117 @@
 <script lang="ts">
 
 import { Component, Vue } from 'vue-property-decorator'
+import mapboxgl from "mapbox-gl";
 
 @Component
 export default class MOWMap extends Vue{
   public routeOptions = [
-    { label: 'Gateway', value: 'gateway' },
-    { label: 'Marcola', value: 'marcola', color: 'green' },
-    { label: 'MC', value: 'mc', color: 'purple' },
-    { label: 'PU', value: 'pu', color: 'orange' },
-    { label: 'Short', value: 'short', color: 'red' },
-    { label: 'Long', value: 'long', color: 'pink' },
-    { label: 'North', value: 'north', color: 'yellow' },
-    { label: 'Will', value: 'will', color: 'indigo' }
+    { label: 'Gateway', value: 'Gateway', color: 'blue', addresses: [
+      {
+        'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [-123.0062619, 44.0538149]
+        }
+      },
+      {
+        'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [-122.9803587, 44.0482189]
+        }
+      },
+      {
+        'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [-122.975543, 44.059392]
+          }
+      }
+    ] 
+    },
+    { label: 'Marcola', value: 'Marcola', color: 'green', addresses: [] },
+    { label: 'MC', value: 'MC', color: 'purple', addresses: [] },
+    { label: 'PU', value: 'PU', color: 'orange', addresses: [] },
+    { label: 'Short', value: 'Short', color: 'red', addresses: [] },
+    { label: 'Long', value: 'Long', color: 'pink', addresses: [] },
+    { label: 'North', value: 'North', color: 'yellow', addresses: [] },
+    { label: 'Will', value: 'Will', color: 'indigo', addresses: [] }
   ]
   public selectedRoutes = ['Gateway', 'Marcola', 'MC', 'PU', 'Short', 'Long', 'North', 'Will']
   public newAddress = ''
   public showWaitlisted = true
+
+  private accessToken = process.env.VUE_APP_MAP_ACCESS_TOKEN
+  private map = {}
+  private center = [-123.0044045, 44.0535664]
+  private zoom = 13
+
+  private gatewayVisibility() {
+    if (this.selectedRoutes.includes('Gateway')) {
+      return 'visible'
+    } else {
+      return 'none'
+    }
+  }
+
+  private createMap() {
+    try {
+      this.map = new mapboxgl.Map({
+        accessToken: this.accessToken,
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: this.center,
+        zoom: this.zoom
+      })
+      this.map.addControl(new mapboxgl.NavigationControl());
+
+      this.map.on('load', () => {
+        for(let route of this.routeOptions) {
+          // Add the vector tileset as a source.
+          this.map.addSource(route.value + 'Routes', {
+            'type': 'geojson',
+            'data': {
+              'type': 'FeatureCollection',
+              'features': route.addresses
+            }
+          });
+          
+          this.map.addLayer({
+            'id': route.value + '-routes',
+            'type': 'circle',
+            'source': route.value + 'Routes',
+            'layout' : {
+              'visibility': "visible"
+            },
+            'paint': {
+              'circle-radius': 6,
+              'circle-color': route.color
+            },
+            'filter': ['==', '$type', 'Point']
+          });
+        }
+      });
+      
+    } catch (err) {
+      console.log("Error rendering map:", err)
+    }
+    
+  }
+
+  mounted() {
+    this.createMap()
+  }
+
+  updateMapVisibility() {
+    for (let route of this.routeOptions) {
+      if (this.selectedRoutes.includes(route.value)) {
+        this.map.setLayoutProperty(route.value + '-routes', 'visibility', 'visible')
+      } else {
+        this.map.setLayoutProperty(route.value + '-routes', 'visibility', 'none')
+      }
+    }
+  }
+
 }
 </script>

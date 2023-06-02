2adb2eb3fd35bd935905ad9eeff772f79edc9a79
Dan Wilson <daniel.gregory.wilson@gmail.com>
2023-06-02 14:33:50 -0700

    Set transition submitter only the first time the transition is updated

--- workflows/api_views.py
+++ workflows/api_views.py
@@ -216,10 +216,13 @@ class EmployeeTransitionViewSet(viewsets.ModelViewSet):
 
     def update(self, request, pk=None):
         t = EmployeeTransition.objects.get(pk=pk)
-        submitter = Employee.objects.get(pk=request.data['submitter_pk']) if \
-            'submitter_pk' in request.data else None
-        t.submitter = submitter
-        t.date_submitted = datetime.now(tz=get_current_timezone())
+
+        # Set submitter only the first time the transition is updated
+        if not t.submitter:
+            submitter = Employee.objects.get(pk=request.data['submitter_pk']) if \
+                'submitter_pk' in request.data else None
+            t.submitter = submitter
+            t.date_submitted = datetime.now(tz=get_current_timezone())
 
         t.type = request.data['type']
         t.employee_first_name = request.data['employee_first_name']

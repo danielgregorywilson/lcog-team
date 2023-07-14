d6065f5bc6f0d7ba1220f9a817a1f3ede78b9ed7
Dan Wilson <daniel.gregory.wilson@gmail.com>
2023-07-13 17:07:59 -0700

    TODO

--- workflows/admin.py
+++ workflows/admin.py
@@ -71,6 +71,7 @@ class StepInstanceForm(ModelForm):
     """Used in StepInstance Admin for process_instance"""
     def __init__(self, *args, **kwargs):
         super(StepInstanceForm, self).__init__(*args, **kwargs)
+        # TODO: This is broken when creating a new StepInstance in the admin
         self.fields['process_instance'].queryset = ProcessInstance.objects.filter(process=self.instance.step.process)
 
 

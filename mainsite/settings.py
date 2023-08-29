a0fef2028e1c1467548b28a9ec4a92d08436e726
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2023-08-29 12:08:06 -0700

    Make Django 4.2 updates

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -142,7 +142,7 @@ WSGI_APPLICATION = 'mainsite.wsgi.application'
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
@@ -243,10 +243,16 @@ if 'STATICFILES_LOCATION' in os.environ:
     # Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
     # you run `collectstatic`).
     STATICFILES_LOCATION = os.environ['STATICFILES_LOCATION']
-    STATICFILES_STORAGE = os.environ['STATICFILES_STORAGE']
-    # Media files location
     MEDIAFILES_LOCATION = os.environ['MEDIAFILES_LOCATION']
-    DEFAULT_FILE_STORAGE = os.environ['DEFAULT_FILE_STORAGE']
+
+    STORAGES = {
+        "default": {
+            "BACKEND": os.environ['DEFAULT_FILE_STORAGE'],
+        },
+        "staticfiles": {
+            "BACKEND": os.environ['STATICFILES_STORAGE'],
+        },
+    }
 
 # Set long timeout for static file browser caching
 AWS_S3_OBJECT_PARAMETERS = {

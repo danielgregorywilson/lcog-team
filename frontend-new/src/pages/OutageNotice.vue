02a16f5b5ace3598a0308d5921db3e4a3ddbd591
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2023-09-06 16:20:59 -0700

    Update message

--- frontend-new/src/pages/OutageNotice.vue
+++ frontend-new/src/pages/OutageNotice.vue
@@ -2,7 +2,7 @@
 <q-layout view="lHh Lpr lFf">
   <q-page-container>
     <q-page>
-      <div class="text-h5 q-ma-xl">RLID.org is experiencing an outage. Our technical teams are working hard to restore services. This work should be complete by 7PM on 9/6/2023. We apologize for the inconvenience.</div>
+      <div class="text-h5 q-ma-xl">The resource you attempted to access is experiencing an outage. Our technical teams are working hard to restore services. We apologize for the inconvenience.</div>
     </q-page>
   </q-page-container>
 </q-layout>

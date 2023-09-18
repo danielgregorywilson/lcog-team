c6d0c8250533de9567eb5f529f0bbc0f586b520b
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2023-09-18 16:11:46 -0700

    Preferch workflow table data to speed up page loads

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -323,6 +323,11 @@ LOGGING = {
             # but still see more verbose logs in the console. See the watchtower
             # documentation for other parameters that can be set here.
             'level': 'DEBUG'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'django-log.log',
         }
     },
     'loggers': {
@@ -331,7 +336,11 @@ LOGGING = {
             'handlers': ['watchtower'],
             'propagate': False,
         },
-        # add your other loggers here...
+        # Log DB queries to a file
+        # 'django.db': {
+        #     'level': 'DEBUG',
+        #     'handlers': ['file'],
+        # }
     },
 }
 

1088d35f2f420d9db08a38f49e9fc6c2e0d73c78
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2023-10-13 12:34:58 -0700

    Expense page done

--- frontend-new/src/pages/purchases/Expenses.vue
+++ frontend-new/src/pages/purchases/Expenses.vue
@@ -6,7 +6,8 @@
       <q-btn color="secondary" icon="west" @click="monthBackward()"/>
       <q-btn color="secondary" icon="east" @click="monthForward()"/>
     </q-btn-group>
-    <q-btn @click="showSubmitToFiscalDialog = true">Submit to Fiscal</q-btn>
+    <q-btn v-if="submitted" @click="showUnsubmitDialog = true">Unsubmit</q-btn>
+    <q-btn v-else @click="showSubmitToFiscalDialog = true">Submit to Fiscal</q-btn>
   </div>
   <q-spinner-grid
     v-if="!calendarLoaded"
@@ -23,30 +24,31 @@
       row-key="name"
       binary-state-sort
       :pagination="pagination"
+      class="expense-table"
     >
       <template v-slot:body="props">
-        <q-tr :props="props">
+        <q-tr :props="props" :class="submitted?'bg-grey':''">
           <q-td key="name" :props="props">
             {{ props.row.name }}
-            <q-popup-edit v-model="props.row.name" buttons v-slot="scope">
+            <q-popup-edit v-if="!submitted" v-model="props.row.name" buttons v-slot="scope">
               <q-input v-model="scope.value" dense autofocus @keyup.enter="scope.set()" />
             </q-popup-edit>
           </q-td>
           <q-td key="date" :props="props">
             {{ readableDate(props.row.date) }}
-            <q-popup-edit v-model="props.row.date" buttons v-slot="scope">
+            <q-popup-edit v-if="!submitted" v-model="props.row.date" buttons v-slot="scope">
               <q-input type="date" v-model="scope.value" dense autofocus @keyup.enter="scope.set()" />
             </q-popup-edit>
           </q-td>
           <q-td key="gl" :props="props">
             <div class="text-pre-wrap">{{ props.row.gl }}</div>
-            <q-popup-edit v-model="props.row.gl" buttons v-slot="scope">
+            <q-popup-edit v-if="!submitted" v-model="props.row.gl" buttons v-slot="scope">
               <q-input v-model="scope.value" dense autofocus @keyup.enter="scope.set()" />
             </q-popup-edit>
           </q-td>
           <q-td key="approver" :props="props">
             {{ props.row.approver.name }}
-            <q-popup-edit v-model="props.row.approver" buttons v-slot="scope">
+            <q-popup-edit v-if="!submitted" v-model="props.row.approver" buttons v-slot="scope">
               <EmployeeSelect
                 name="approver"
                 label="Approver"
@@ -62,7 +64,7 @@
           </q-td>
           <q-td key="receipt" :props="props">
             {{ props.row.receipt }}
-            <q-popup-edit v-model="props.row.receipt" buttons>
+            <q-popup-edit v-if="!submitted" v-model="props.row.receipt" buttons>
               <FileUploader
                 label="Receipt"
                 :file="props.row.receipt"
@@ -74,7 +76,7 @@
           </q-td>
         </q-tr>
       </template>
-      <template v-slot:bottom-row>
+      <template v-slot:bottom-row v-if="!submitted">
         <q-tr @click="clickAddExpense()" class="cursor-pointer row-add-new">
           <q-td colspan="100%">
             <q-icon name="add" size="md" class="q-pr-sm"/>New Expense
@@ -86,57 +88,87 @@
 
   <!-- Submit to Fiscal Dialog -->
   <q-dialog v-model="showSubmitToFiscalDialog">
-      <q-card class="q-pa-md" style="width: 400px">
-        <div class="text-h6">Submit {{monthDisplay()}} expenses to Fiscal?</div>
-        <q-form
-          @submit='onSubmitFiscalDialog()'
-          class="q-gutter-md"
-        >
-          <q-input
-            v-model="sendDialogMessage"
-            filled
-            type="textarea"
-            label="Extra message to include"
+    <q-card class="q-pa-md" style="width: 400px">
+      <div class="text-h6">Submit {{monthDisplay()}} expenses to Fiscal?</div>
+      <q-form
+        @submit='onSubmitFiscalDialog()'
+        class="q-gutter-md"
+      >
+        <q-input
+          v-model="sendDialogMessage"
+          filled
+          type="textarea"
+          label="Extra message to include"
+        />
+        <div class="row justify-between">
+          <q-btn
+            name="send-fiscal-dialog-button"
+            label="Send"
+            icon-right="send"
+            type="submit"
+            color="primary"
+            :disable="formErrors()"
           />
-          <div class="row justify-between">
-            <q-btn
-              name="send-fiscal-dialog-button"
-              label="Send"
-              icon-right="send"
-              type="submit"
-              color="primary"
-              :disable="formErrors()"
-            />
-            <!-- <div
-              v-if="formErrors()"
-              class="text-red text-bold"
-              style="width:180px;"
-            >
-              There are errors in the form. Fix before submitting.
-            </div> -->
-          </div>
-        </q-form>
-      </q-card>
-    </q-dialog>
+          <!-- <div
+            v-if="formErrors()"
+            class="text-red text-bold"
+            style="width:180px;"
+          >
+            There are errors in the form. Fix before submitting.
+          </div> -->
+        </div>
+      </q-form>
+    </q-card>
+  </q-dialog>
+
+  <!-- Unsubmit Dialog -->
+  <q-dialog v-model="showUnsubmitDialog">
+    <q-card class="q-pa-md" style="width: 400px">
+      <div class="text-h6">Unsubmit {{monthDisplay()}} expenses?</div>
+      <q-form
+        @submit='onUnsubmitDialog()'
+        class="q-gutter-md"
+      >
+        <div class="row justify-between">
+          <q-btn
+            name="unsubmit-dialog-button"
+            label="Unsubmit"
+            icon-right="cancel"
+            type="submit"
+            color="primary"
+          />
+        </div>
+      </q-form>
+    </q-card>
+  </q-dialog>
 </q-page>
 </template>
 
-<script setup lang="ts">
+<style scoped lang="scss">
+  // .expense-table {
+  //   width: 221px
+  // }
+</style>
 
+<script setup lang="ts">
 import { onMounted, ref } from 'vue'
+import { useQuasar } from 'quasar'
 import EmployeeSelect from 'src/components/EmployeeSelect.vue'
 import FileUploader from 'src/components/FileUploader.vue'
 import { readableDate } from 'src/filters'
 import { TimeOffRequestRetrieve } from 'src/types'
 import { useTimeOffStore } from 'src/stores/timeoff'
 
-type TimeOffCalendarData = Array<{date: string, isToday: boolean, requests: Array<TimeOffRequestRetrieve>}>
+type Expense = {date: string, isToday: boolean}
 
+const quasar = useQuasar()
 const timeOffStore = useTimeOffStore()
 
+let submitted = ref(false)
 let calendarLoaded = ref(true)
 let showSubmitToFiscalDialog = ref(false)
 let sendDialogMessage = ref('')
+let showUnsubmitDialog = ref(false)
 
 let today = ref(new Date())
 let firstOfThisMonth = ref(new Date())
@@ -193,37 +225,38 @@ const rows = ref([
   }
 ])
 
-function monthExpenses(): TimeOffCalendarData {
-  const apiResults = timeOffStore.teamTimeOffRequests
-  let sortedTimeOff: TimeOffCalendarData = []
-  if (apiResults) {
-    for (let i=0; i<5; i++) {
-      let d = new Date(selectedMonday.value.getTime() + i*(1000 * 60 * 60 * 24))
-      let isToday = d.setHours(0,0,0,0) === today.value.setHours(0,0,0,0)
-      sortedTimeOff.push({
-        date: d.toLocaleDateString('en-us', { weekday: 'long', month: 'long', day: 'numeric' }),
-        isToday: isToday,
-        requests: apiResults.filter(request => {
-          const targetDateMS = d.setHours(0,0,0,0)
+function monthExpenses(): Expense[] {
+  return []
+  // const apiResults = timeOffStore.teamTimeOffRequests
+  // let sortedTimeOff: TimeOffCalendarData = []
+  // if (apiResults) {
+  //   for (let i=0; i<5; i++) {
+  //     let d = new Date(selectedMonday.value.getTime() + i*(1000 * 60 * 60 * 24))
+  //     let isToday = d.setHours(0,0,0,0) === today.value.setHours(0,0,0,0)
+  //     sortedTimeOff.push({
+  //       date: d.toLocaleDateString('en-us', { weekday: 'long', month: 'long', day: 'numeric' }),
+  //       isToday: isToday,
+  //       requests: apiResults.filter(request => {
+  //         const targetDateMS = d.setHours(0,0,0,0)
 
-          const fromDate = new Date(request.start_date)
-          const fromTZOffset = fromDate.getTimezoneOffset() * 60000
-          const fromDateMS = new Date(fromDate.getTime() + fromTZOffset).setHours(0,0,0,0)
+  //         const fromDate = new Date(request.start_date)
+  //         const fromTZOffset = fromDate.getTimezoneOffset() * 60000
+  //         const fromDateMS = new Date(fromDate.getTime() + fromTZOffset).setHours(0,0,0,0)
 
-          const toDate = new Date(request.end_date)
-          const toTZOffset = toDate.getTimezoneOffset() * 60000
-          const toDateMS = new Date(toDate.getTime() + toTZOffset).setHours(0,0,0,0)
+  //         const toDate = new Date(request.end_date)
+  //         const toTZOffset = toDate.getTimezoneOffset() * 60000
+  //         const toDateMS = new Date(toDate.getTime() + toTZOffset).setHours(0,0,0,0)
 
-          if (fromDateMS <= targetDateMS && targetDateMS <= toDateMS) {
-            return true
-          } else {
-            return false
-          }
-        })
-      })
-    }
-  }
-  return sortedTimeOff
+  //         if (fromDateMS <= targetDateMS && targetDateMS <= toDateMS) {
+  //           return true
+  //         } else {
+  //           return false
+  //         }
+  //       })
+  //     })
+  //   }
+  // }
+  // return sortedTimeOff
 }
 
 function monthDisplay(): string {
@@ -273,6 +306,7 @@ function clickAddExpense(): void {
     approver: '',
     receipt: ''
   })
+}
 
 function formErrorItems(): Array<[string, string]> {
   let errorItems: Array<[string, string]> = []
@@ -297,52 +331,62 @@ function formErrors() {
 }
 
 function onSubmitFiscalDialog() {
-  const extraMessage = type == 'ASSIGN' ? reassignDialogMessage.value : sendDialogMessage.value
-  workflowsStore.sendTransitionToEmailList(transitionPk.value, {
-    type: type,
-    reassignTo: assignee.value,
-    update: sendDialogUpdate.value,
-    extraMessage,
-    senderName: userStore.getEmployeeProfile.name,
-    senderEmail: userStore.getEmployeeProfile.email,
-    transitionUrl: route.fullPath
-  })
-    .then(() => {
-      quasar.notify({
-        message: 'Sent',
-        color: 'positive',
-        icon: 'send'
-      })
-      showSendToSDSHiringLeadsDialog.value = false
-      showSendToFiscalDialog.value = false
-      showSendToHRDialog.value = false
-      showSendToSTNDialog.value = false
-      showAssigneeDialog.value = false
-      sendDialogUpdate.value = false
-      sendDialogMessage.value = ''
-      reassignDialogMessage.value = ''
-      // Signal to WorkflowInstanceDetail that the transition was assigned or
-      // completed, in which case we need to get the newly created process
-      // instances.
-      bus.emit('transitionReassigned', Math.random())
-    })
-    .catch(e => {
-      console.error('Error sending email', e)
-      quasar.notify({
-        message: 'Error sending email',
-        color: 'negative',
-        icon: 'report_problem'
-      })
-    })
-}
-
-  // workflowsStore.createNewEmployeeOnboarding()
-  //   .then((wfi) => {
-  //     navigateToWorkflowTransitionForm(wfi.pk)
+  // const extraMessage = type == 'ASSIGN' ? reassignDialogMessage.value : sendDialogMessage.value
+  // workflowsStore.sendTransitionToEmailList(transitionPk.value, {
+  //   type: type,
+  //   reassignTo: assignee.value,
+  //   update: sendDialogUpdate.value,
+  //   extraMessage,
+  //   senderName: userStore.getEmployeeProfile.name,
+  //   senderEmail: userStore.getEmployeeProfile.email,
+  //   transitionUrl: route.fullPath
+  // })
+  //   .then(() => {
+  //     quasar.notify({
+  //       message: 'Sent',
+  //       color: 'positive',
+  //       icon: 'send'
+  //     })
+  //     showSendToSDSHiringLeadsDialog.value = false
+  //     showSendToFiscalDialog.value = false
+  //     showSendToHRDialog.value = false
+  //     showSendToSTNDialog.value = false
+  //     showAssigneeDialog.value = false
+  //     sendDialogUpdate.value = false
+  //     sendDialogMessage.value = ''
+  //     reassignDialogMessage.value = ''
+  //     // Signal to WorkflowInstanceDetail that the transition was assigned or
+  //     // completed, in which case we need to get the newly created process
+  //     // instances.
+  //     bus.emit('transitionReassigned', Math.random())
   //   })
   //   .catch(e => {
-  //     console.error('Error creating a new employee onboarding workflow instance', e)
+  //     console.error('Error sending email', e)
+  //     quasar.notify({
+  //       message: 'Error sending email',
+  //       color: 'negative',
+  //       icon: 'report_problem'
+  //     })
   //   })
+  showSubmitToFiscalDialog.value = false
+  submitted.value = true
+  rows.value.forEach(row => row.submitted = true)
+  quasar.notify({
+    message: 'Sent',
+    color: 'positive',
+    icon: 'send'
+  })
+}
+
+function onUnsubmitDialog() {
+  showUnsubmitDialog.value = false
+  submitted.value = false
+  rows.value.forEach(row => row.submitted = false)
+  quasar.notify({
+    message: 'Unsubmitted',
+    color: 'positive',
+    icon: 'cancel'
+  })
 }
 
 onMounted(() => {

6df3a12d26cb586b551062e48406cb454642d7d6
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2023-12-20 15:43:41 -0800

    Remove Quasar 1 frontend

--- frontend/src/store/modules/people/state.ts
+++
@@ -1,9 +0,0 @@
-import { PeopleStateInterface } from 'src/store/types';
-
-const state: PeopleStateInterface = {
-  simpleEmployeeList: [],
-  simpleEmployeeDetail: { pk: -1, name: '' },
-  unitList: [],
-};
-
-export default state;

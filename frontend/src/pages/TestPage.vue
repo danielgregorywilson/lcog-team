8aba3d455c5f86bc7e2e4db14419e902a9519e17
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2023-12-22 16:37:36 -0800

    Test page

---
+++ frontend/src/pages/TestPage.vue
@@ -0,0 +1,27 @@
+<template>
+<q-layout view="lHh Lpr lFf">
+  <q-page-container>
+    <q-page>
+      Test
+    </q-page>
+  </q-page-container>
+</q-layout>
+</template>
+
+<style lang="scss">
+</style>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+import { useTimeOffStore } from 'src/stores/timeoff'
+import { useWorkflowsStore } from 'src/stores/workflows'
+
+const timeOffStore = useTimeOffStore()
+const workflowsStore = useWorkflowsStore()
+
+onMounted(() => {
+  // workflowsStore.getWorkflows({archived: false, complete: false})
+  timeOffStore.getTeamTimeOffRequests()
+})
+</script>

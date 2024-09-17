db2906e89340e0076dc53b57c118b19b183c2dac
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2024-09-17 16:38:01 -0700

    Whitespace

--- frontend/src/components/workflows/ProcessInstanceDetail.vue
+++ frontend/src/components/workflows/ProcessInstanceDetail.vue
@@ -18,8 +18,15 @@
         :done="!!si.completed_at"
       >
         <div>{{ si.step.description }}</div>
-        <StepAction v-for="action of si.step.optional_actions" :action="action" :key="action.pk" />
-        <div v-if="si.completed_at" class="text-secondary">Completed by {{ si.completed_by_name }} on {{ formatDate(si.completed_at, 'dddd, M/D/YY [at] HH:MM') }}</div>
+        <StepAction
+          v-for="action of si.step.optional_actions"
+          :action="action"
+          :key="action.pk"
+        />
+        <div v-if="si.completed_at" class="text-secondary">
+          Completed by {{ si.completed_by_name }} on
+          {{ formatDate(si.completed_at, 'dddd, M/D/YY [at] HH:MM') }}
+        </div>
         <q-stepper-navigation v-if="stepInstanceIsComplete(si)">
           <div v-if="si.undo_completion_possible">
             <q-btn
@@ -85,7 +92,8 @@ function latestStepInstance() {
 }
 
 function setCurrentStepInstance() {
-  currentStepInstance.value = workflowsStore.processInstanceCurrentStepPks[props.pi.pk]
+  currentStepInstance.value =
+    workflowsStore.processInstanceCurrentStepPks[props.pi.pk]
 }
 
 function stepInstanceIsComplete(stepInstance: StepInstance): boolean {
@@ -96,22 +104,26 @@ function stepInstanceIsComplete(stepInstance: StepInstance): boolean {
   }
 }
 
-function userAllowedToCompleteStepInstance(stepInstance: StepInstance): boolean {
+function userMayCompleteStepInstance(stepInstance: StepInstance): boolean {
   const step = stepInstance.step
   if (userStore.getEmployeeProfile.is_all_workflows_admin) {
     // If they are an All-Workflows-Admin, allow completion
     return true
   } else if (step.workflow_role_pk) {
     // If they are an admin of the workflow, allow completion
-    return userStore.getEmployeeProfile.workflow_roles.indexOf(step.workflow_role_pk) != -1
+    return userStore.getEmployeeProfile.workflow_roles
+      .indexOf(step.workflow_role_pk) != -1
   } else if (step.process_role_pk) {
     // If they are an admin of the process, allow completion
-    return userStore.getEmployeeProfile.workflow_roles.indexOf(step.process_role_pk) != -1
+    return userStore.getEmployeeProfile.workflow_roles
+      .indexOf(step.process_role_pk) != -1
   } else if (step.role) {
     // If they are assigned to the step's role, allow completion
-    return userStore.getEmployeeProfile.workflow_roles.indexOf(step.role.pk) != -1
+    return userStore.getEmployeeProfile.workflow_roles
+      .indexOf(step.role.pk) != -1
   } else {
-    // TODO: What should happen if no role assigned? Only admins? Everyone? Require all steps to have roles?
+    // TODO: What should happen if no role assigned? Only admins? Everyone?
+    // Require all steps to have roles?
     return true
   }
 }
@@ -120,14 +132,14 @@ function canCompleteStepInstance(stepInstance: StepInstance): boolean {
   if (stepInstance.completed_at) {
     return false
   }
-  return userAllowedToCompleteStepInstance(stepInstance)
+  return userMayCompleteStepInstance(stepInstance)
 }
 
 function canUndoStepCompletion(stepInstance: StepInstance): boolean {
   if (!stepInstance.completed_at) {
     return false
   }
-  return userAllowedToCompleteStepInstance(stepInstance)
+  return userMayCompleteStepInstance(stepInstance)
 }
 
 function completeStep(stepInstancePk: number, nextStepPk?: number): void {

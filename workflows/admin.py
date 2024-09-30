68e52c47912b271788a0af7aa17b0a1065e6e77c
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2024-09-30 16:55:26 -0700

    Whitespace

--- workflows/admin.py
+++ workflows/admin.py
@@ -42,32 +42,49 @@ class StepForm(ModelForm):
     """Used in Step Admin for next_step"""
     def __init__(self, *args, **kwargs):
         super(StepForm, self).__init__(*args, **kwargs)
-        self.fields['next_step'].queryset = Step.objects.filter(process=self.instance.process).exclude(pk=self.instance.pk).order_by('order')
+        self.fields['next_step'].queryset = Step.objects.filter(
+            process=self.instance.process
+        ).exclude(pk=self.instance.pk).order_by('order')
 
 
 class StepInlineForm(ModelForm):
-    """Used in Process Admin for child Steps' next_steps and trigger_processes"""
+    """
+    Used in Process Admin for child Steps' next_steps and trigger_processes
+    """
     def __init__(self, *args, parent_object, **kwargs):
         super(StepInlineForm, self).__init__(*args, **kwargs)
-        self.fields['next_step'].queryset = Step.objects.filter(process=parent_object).exclude(pk=self.instance.pk).order_by('order')
+        self.fields['next_step'].queryset = Step.objects.filter(
+            process=parent_object
+            ).exclude(pk=self.instance.pk).order_by('order')
         if (parent_object.pk):
-            self.fields['trigger_processes'].queryset = Process.objects.filter(workflow=parent_object.workflow).exclude(pk=parent_object.pk).order_by('name')
+            self.fields['trigger_processes'].queryset = Process.objects.filter(
+                workflow=parent_object.workflow
+            ).exclude(pk=parent_object.pk).order_by('name')
 
 
 class StepChoiceForm(ModelForm):
     """Used in Step Admin for Step choices"""
     def __init__(self, *args, parent_object, **kwargs):
         super(StepChoiceForm, self).__init__(*args, **kwargs)
-        self.fields['next_step'].queryset = Step.objects.filter(process=parent_object.process).exclude(pk=parent_object.pk).order_by('order')         
+        self.fields['next_step'].queryset = Step.objects.filter(
+            process=parent_object.process
+        ).exclude(pk=parent_object.pk).order_by('order')         
 
 
 class ProcessInstanceForm(ModelForm):
-    """Used in ProcessInstance Admin for current_step_instance and workflow_instance"""
+    """
+    Used in ProcessInstance Admin for current_step_instance and
+    workflow_instance
+    """
     def __init__(self, *args, **kwargs):
         super(ProcessInstanceForm, self).__init__(*args, **kwargs)
-        self.fields['current_step_instance'].queryset = StepInstance.objects.filter(process_instance=self.instance)
+        self.fields['current_step_instance'].queryset = \
+            StepInstance.objects.filter(process_instance=self.instance)
         if hasattr(self.instance, 'process'):
-            self.fields['workflow_instance'].queryset = WorkflowInstance.objects.filter(workflow=self.instance.process.workflow)
+            self.fields['workflow_instance'].queryset = \
+                WorkflowInstance.objects.filter(
+                    workflow=self.instance.process.workflow
+                )
 
 
 class StepInstanceForm(ModelForm):
@@ -75,7 +92,8 @@ class StepInstanceForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(StepInstanceForm, self).__init__(*args, **kwargs)
         # TODO: This is broken when creating a new StepInstance in the admin
-        self.fields['process_instance'].queryset = ProcessInstance.objects.filter(process=self.instance.step.process)
+        self.fields['process_instance'].queryset = \
+            ProcessInstance.objects.filter(process=self.instance.step.process)
 
 
 class StepInline(admin.TabularInline):
@@ -88,7 +106,8 @@ class StepInline(admin.TabularInline):
 
     def edit_link(self, instance):
         url = reverse('admin:%s_%s_change' % (
-            instance._meta.app_label,  instance._meta.model_name),  args=[instance.pk] )
+            instance._meta.app_label, instance._meta.model_name
+        ), args=[instance.pk] )
         if instance.pk:
             return mark_safe(u'<a href="{u}">edit</a>'.format(u=url))
         else:
@@ -101,7 +120,8 @@ class StepInline(admin.TabularInline):
         ):
             if idx != 0:
                 choices_text += " / "
-            choices_text += choice.choice_text + ": " + str(choice.next_step.order) + ' - ' + choice.next_step.name
+            choices_text += choice.choice_text + ": " + \
+                str(choice.next_step.order) + ' - ' + choice.next_step.name
         return choices_text
 
 
@@ -138,16 +158,27 @@ class ActionAdmin(admin.ModelAdmin):
 
 @admin.register(WorkflowInstance)
 class WorkflowInstanceAdmin(admin.ModelAdmin):
-    list_display = ("pk", "workflow", "created_by", "started_at", "completed_at", "complete", "active")
+    list_display = (
+        "pk", "workflow", "created_by", "started_at", "completed_at",
+        "complete", "active"
+    )
     list_filter = ("workflow", "active", "complete")
     readonly_fields = ("started_at",)
-    fields = ("created_by", "started_at", "completed_at", "workflow", "transition", "active", "complete")
+    fields = (
+        "created_by", "started_at", "completed_at", "workflow", "transition",
+        "active", "complete"
+    )
 
 
 @admin.register(ProcessInstance)
 class ProcessInstanceAdmin(admin.ModelAdmin):
-    list_display = ("pk", "process", "workflow_instance", "current_step_instance")
-    fields = ("started_at", "completed_at", "process", "workflow_instance", "current_step_instance")
+    list_display = (
+        "pk", "process", "workflow_instance", "current_step_instance"
+    )
+    fields = (
+        "started_at", "completed_at", "process", "workflow_instance",
+        "current_step_instance"
+    )
     readonly_fields = ("started_at",)
     form = ProcessInstanceForm
 
@@ -156,7 +187,10 @@ class ProcessInstanceAdmin(admin.ModelAdmin):
 class StepInstanceAdmin(admin.ModelAdmin):
     list_display = ("pk", "step", "process_instance")
     ordering = ("process_instance", "step__order")
-    fields = ("started_at", "completed_at", "step", "process_instance", "completed_by")
+    fields = (
+        "started_at", "completed_at", "step", "process_instance",
+        "completed_by"
+    )
     readonly_fields = ("started_at",)
     form = StepInstanceForm
 

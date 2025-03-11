3f0fb4e1c3830fa4f4d76f338b79ef03430092ac
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2025-03-11 12:34:59 -0700

    Marketing page WIP

---
+++ frontend/src/pages/MarketingPage.vue
@@ -0,0 +1,67 @@
+<template>
+<q-layout view="lHh Lpr lFf">
+  <q-page-container>
+    <q-page>
+      <div class="text-h4 q-pa-sm q-pl-md">√ Product Title</div>
+    <div id="hero-image" class="row items-center justify-center">
+      <div>^^^ MOUNTAINS ^^^</div>
+    </div>
+    <div id="hero-text" class="row items-center q-pl-md">
+      <div class="col">
+        <div>Headline!</div>
+        <div>A lot of text. A lot of text. A lot of text. A lot of text. A lot of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text.</div>
+      </div>
+      <div class="col">
+        <div class="row items-center justify-center">
+          <q-btn>Get Involved</q-btn>
+        </div>
+      </div>
+    </div>
+    <div id="hero-subtext" class="row items-center">
+      <div class="col"></div>
+      <div class="col">
+        <div class="row justify-end">Headline!</div>
+        <div class="row justify-end">A lot of text. A lot of text. A lot of text. A lot of text. A lot of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text.</div>
+      </div>
+    </div>
+    <div class="row product items-center q-pl-md">
+      <div class="col">
+        <div>Workflows</div>
+        <div>A lot of text. A lot of text. A lot of text. A lot of text. A lot of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text. A whole lotta of text.</div>
+      </div>
+      <div class="col">
+        <div class="row items-center justify-center">
+          <q-btn>Get Involved</q-btn>
+        </div>
+      </div>
+    </div>
+    </q-page>
+  </q-page-container>
+</q-layout>
+</template>
+
+<style lang="scss">
+.q-header {
+  display: none;
+}
+
+#hero-image {
+  background-color: green;
+  height: 150px;
+}
+
+#hero-text {
+  background-color: lightgreen;
+  height: 200px;
+}
+
+#hero-subtext {
+  background-color: lightyellow;
+  height: 150px;
+}
+
+.product {
+  background-color: pink;
+  height: 300px;
+}
+</style>

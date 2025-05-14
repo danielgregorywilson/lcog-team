6261f5eed9647af83242067fc3bca940b185fdc3
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2025-05-14 15:07:07 -0700

    TEST

--- deskreservation/api_views.py
+++ deskreservation/api_views.py
@@ -27,6 +27,7 @@ class DeskViewSet(viewsets.ModelViewSet):
         """
         user = self.request.user
         is_trusted_ip = get_is_trusted_ip()
+        is_trusted_ip = True
         if user.is_authenticated or is_trusted_ip:
             queryset = Desk.active_objects.all()
             building = self.request.query_params.get('building', None)
@@ -55,6 +56,7 @@ class DeskReservationViewSet(viewsets.ModelViewSet):
         """
         user = self.request.user
         is_trusted_ip = get_is_trusted_ip()
+        is_trusted_ip = True
         if user.is_authenticated or is_trusted_ip:
             queryset = DeskReservation.currently_reserved_objects.all()
             employee = self.request.query_params.get('employee', None)
